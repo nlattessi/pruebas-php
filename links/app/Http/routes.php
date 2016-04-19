b145e8e51b5ea5e143e991f377958b922509e2a2
Nahuel Lattessi <n_lattessi@hotmail.com>
2016-04-19 11:13:12 -0300

    end links

--- links/app/Http/routes.php
+++ links/app/Http/routes.php
@@ -16,6 +16,32 @@ Route::get('/', function () {
     return view('welcome', compact('links'));
 });
 
+Route::get('/submit', function () {
+    return view('submit');
+});
+
+Route::post('/submit', function(\Illuminate\Http\Request $request) {
+    $validator = Validator::make($request->all(), [
+        'title' => 'required|max:255',
+        'url' => 'required|max:255',
+        'description' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return back()
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $link = new \App\Link;
+    $link->title = $request->title;
+    $link->url = $request->url;
+    $link->description = $request->description;
+    $link->save();
+
+    return redirect('/');
+});
+
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
--- links/tests/features/LinkTest.php
+++ links/tests/features/LinkTest.php
@@ -14,4 +14,29 @@ class LinkTest extends TestCase
         $this->visit('/')
              ->see('dotdev.co');
     }
+
+    public function testWeSeeLinksForm()
+    {
+        $this->visit('/submit')
+            ->see('Submit a link');
+    }
+
+    public function testLinksFormValidation()
+    {
+        $this->visit('/submit')
+            ->press('Submit')
+            ->see('The title field is required')
+            ->see('The url field is required')
+            ->see('The description field is required');
+    }
+
+    public function testSubmitLinksToDb()
+    {
+        $this->visit('/submit')
+            ->type('dotdev', 'title')
+            ->type('https://dotdev.co', 'url')
+            ->type('My description', 'description')
+            ->press('Submit')
+            ->seeInDatabase('links', ['title' => 'dotdev']);
+    }
 }
