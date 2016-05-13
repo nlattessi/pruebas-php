5e00151f64f5258a6fc37782010c791397147e39
Nahuel Lattessi <n_lattessi@hotmail.com>
2016-05-13 00:46:55 -0300

    SlimAuth tutorial slim3

---
+++ SlimAuth/bootstrap/app.php
@@ -0,0 +1,15 @@
+<?php
+
+session_start();
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = new \Slim\App([
+    'settings' => [
+        'displayErrorDetails' => true,
+    ]
+]);
+
+$container = $app->getContainer();
+
+require __DIR__ . '/../app/routes.php';
