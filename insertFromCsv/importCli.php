f23517902ab0e411d96cb832f5a0bfbb4d4ea7f8
Nahuel Lattessi <n_lattessi@hotmail.com>
2016-05-13 15:52:14 -0300

    update

--- insertFromCsv/importCli.php
+++ insertFromCsv/importCli.php
@@ -12,7 +12,7 @@ if (($handle = fopen("choferes.csv", "r")) !== FALSE) {
 
         while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
             if ($data[0]) {
-                $stmt = $conn->prepare("INSERT INTO chofer (nombre, apellido, dni, tiene_curso_basico) SELECT * FROM (SELECT :nombre, :apellido, :dni, 1) AS tmp WHERE NOT EXISTS (SELECT dni FROM chofer WHERE dni = :dni2) LIMIT 1");
+                $stmt = $conn->prepare("INSERT INTO chofer (nombre, apellido, dni, tiene_curso_basico) SELECT * FROM (SELECT :nombre as nombre, :apellido as apellido, :dni, 1) AS tmp WHERE NOT EXISTS (SELECT ch.dni FROM chofer as ch WHERE ch.dni = :dni2) LIMIT 1");
                 $stmt->bindParam(':nombre', trim($data[0]));
                 $stmt->bindParam(':apellido', trim($data[1]));
                 $stmt->bindParam(':dni', trim($data[2]));
