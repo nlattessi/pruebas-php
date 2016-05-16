d0b17e5135a4a23934b2c62a27cb1d00d14a4d37
Nahuel Lattessi <n_lattessi@hotmail.com>
2016-05-16 01:09:14 -0300

    video 14

--- SlimAuth/bootstrap/app.php
+++ SlimAuth/bootstrap/app.php
@@ -7,10 +7,27 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = new \Slim\App([
     'settings' => [
         'displayErrorDetails' => true,
-    ]
+        'db' => [
+            'driver' => 'mysql',
+            'host' => '127.0.0.1',
+            'database' => 'SlimAuth',
+            'username' => 'root',
+            'password' => 'qwerty',
+            'charset' => 'utf8',
+        ],
+    ],
 ]);
 
 $container = $app->getContainer();
+
+$capsule = new \Illuminate\Database\Capsule\Manager;
+$capsule->addConnection($container['settings']['db']);
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+$container['db'] = function ($container) use ($capsule) {
+    return $capsule;
+};
 
 $container['view'] = function ($container) {
 	$view = new \Slim\Views\Twig(__DIR__ . '/../resources/views', [
@@ -25,8 +42,20 @@ $container['view'] = function ($container) {
 	return $view;
 };
 
+$container['validator'] = function ($container) {
+    return new App\Validation\Validator;
+};
+
 $container['HomeController'] = function ($container) {
 	return new \App\Controllers\HomeController($container);
 };
+
+$container['AuthController'] = function ($container) {
+    return new \App\Controllers\Auth\AuthController($container);
+};
+
+$app->add(new \App\Middleware\ValidationErrorsMiddleware($container));
+
+$app->add(new \App\Middleware\OldInputMiddleware($container));
 
 require __DIR__ . '/../app/routes.php';
